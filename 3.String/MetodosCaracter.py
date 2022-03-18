fe26d4404a66576ec72a9430968ed56374ca3f9f
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-03-18 12:19:43 -0500

    string upper lower

--- 3.String/MetodosCaracter.py
+++ 3.String/MetodosCaracter.py
@@ -1,2 +1,10 @@
-cadena = 'Esoy utilizando los metodos de Pythion'
+cadena = 'Estoy utilizando los metodos de Python'
+cadena2 = 'ESTOY UTILIZANDO LOS METODOS EN PYTHON'
 print(cadena.lower())
+print(cadena2.lower())
+
+print(cadena.upper())
+
+print(cadena.capitalize())
+print(cadena.title())
+print(cadena.swapcase())
