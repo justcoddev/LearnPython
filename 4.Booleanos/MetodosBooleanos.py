8f9cb9f530bc391098182042d4db0218a4300bd8
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-03-20 00:03:31 -0500

    metodos booleanos

---
+++ 4.Booleanos/MetodosBooleanos.py
@@ -0,0 +1,9 @@
+cadena = 'Estoy mostrando los metodos booleanos para las Strings'
+cadena2 = 'ESTOY MOSTRANDO LOS METODOS BOOLEANOS PARA LAS STRINGS'
+cadena3 = 'estoy mostrando los metodos booleanos para las strings'
+print(cadena.startswith('E'))
+print(cadena2.endswith('S'))
+
+print(cadena2.isupper())
+
+print(cadena3.islower())
