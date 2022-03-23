18ad0cbd57701e31e5067e5c5f31dc8ac0ab73d7
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-03-23 09:17:51 -0500

    metodos listas 3

---
+++ 7.Listas/MetodosListas3.py
@@ -0,0 +1,14 @@
+lista = ['Python', 24 ,'Teddy', 'Summers', 12]
+
+# lista[3] = 'T3ddy'
+# print(lista)
+# print(lista[3])
+
+# lista.pop()
+# print(lista)
+# lista.pop()
+# print(lista)
+
+
+lista.remove('Python')
+print(lista)
