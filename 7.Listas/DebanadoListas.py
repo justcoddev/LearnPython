dc63d6d7c4df2422adbea0ee9ec42236df8dace8
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-03-22 22:47:29 -0500

    Listas metodos 1 y 2

---
+++ 7.Listas/DebanadoListas.py
@@ -0,0 +1,8 @@
+lista = [10, 20, 3.14, 'Teddy', 'Summers', 7, 'Estudiante', 'Lapiz']
+
+print(lista[5])
+print(lista[0:5])
+print(lista[:2])
+print(lista[1:])
+print(lista[-1])
+print(lista[-5:-2])
---
+++ 7.Listas/MetodosListas2.py
@@ -0,0 +1,10 @@
+lista = [5, 4, 2, 1, 3]
+print(lista)
+
+print(lista.count(5))
+print(lista.index(1))
+
+lista.sort()
+print(lista)
+lista.reverse()
+print(lista)
