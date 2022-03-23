2e16d5be2dbd7cffe16d12257a6b021f99e47e0a
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-03-23 09:25:01 -0500

    metodo append

---
+++ 7.Listas/LlenarLista.py
@@ -0,0 +1,14 @@
+lista = [1, 2, 3]
+lista2 = [4, 5, 6]
+
+lista3 = lista + lista2
+print(lista3)
+print('Eso es una lsita de distintos datos: ',lista)
+
+
+
+lista4 = []
+
+edad = int(input('Ingresa tu edad: '))
+lista4.append(edad)
+print(lista4)
