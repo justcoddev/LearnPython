54f47db0c2a6ca455e8eba807421fe1eaee48e11
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-04-13 23:52:26 -0500

    Elementos Diccionario

---
+++ 8.Diccionarios/ElementosDiccionario.py
@@ -0,0 +1,11 @@
+diccionario = {'Nombre' : "Teddy", 'Apellido' : 'Summers', 'Estatura' : 1.80}
+
+print(diccionario)
+print(diccionario.keys()) # TODO: mostrar llaves(claves)
+print(diccionario.values())  # TODO: mostrar valores del diccionario
+
+print(diccionario["Estatura"])
+diccionario["Peso"] = "58 kg"  # TODO:  agregar nuevo 
+print(diccionario)
+diccionario['Nombre'] = "teddy" # TODO: Cambiar nombre
+print(diccionario)
