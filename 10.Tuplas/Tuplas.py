afa61175443f4e1cdcce6cca3ffd09829a2525e5
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-04-17 17:20:31 -0500

    tuplas

---
+++ 10.Tuplas/Tuplas.py
@@ -0,0 +1,11 @@
+# TODO: una tupla puede o no llevar parentesis, como buena práctica es mejor ()
+tupla = (1, 2, 3, 4, 5)
+tupla2 = (6, 7, 8, 9, 10)
+print(tupla)
+print(type(tupla))
+print(tupla[2]) # TODO: mostrar dato de tupla
+print(tupla[0:3])  # TODO:  devanado de tuplas
+print(tupla+tupla2) # TODO: cocatenar tuplas
+
+tupla[2] = 10
+print(tupla)
