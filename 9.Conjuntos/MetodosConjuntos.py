18d94bb7b31567af6bd7c9ed7d699561f0ce64f2
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-04-17 17:11:38 -0500

    metodos conjuntos

---
+++ 9.Conjuntos/MetodosConjuntos.py
@@ -0,0 +1,26 @@
+conjunto = {1, 2, 3, 4, 5}
+print(conjunto)
+conjunto.add(20)  # TODO:  añadir nuevo dato al conjunto
+print(conjunto)
+
+conjunto.remove(1)  # TODO:  elimina un dato
+conjunto.discard(3)  # TODO:  elimina un dato
+print(conjunto)
+
+
+conjunto = {1, 2, 3, 4, 5}
+print(conjunto)
+conjunto.pop()  # TODO:  elima un valor al azar
+print(conjunto)
+
+
+conjunto = {1, 2, 3, 4, 5}
+print(conjunto)
+# TODO: permite actualizar conjunto pero si repetirl datos
+conjunto.update([1, 2, 3, 6])
+print(conjunto)
+
+conjunto = {1, 2, 3, 4, 5}
+print(conjunto)
+conjunto.clear() # TODO:  deja el conjunto vacio
+print(conjunto)
