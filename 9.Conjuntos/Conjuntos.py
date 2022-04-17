f3bf2209e259782b2a03eb29391f441d1cb61d78
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-04-17 17:11:17 -0500

    conjuntos

---
+++ 9.Conjuntos/Conjuntos.py
@@ -0,0 +1,9 @@
+conjunto = {1, 2, 3, 4, 5}
+lista = [1, 1, 2, 3, 4, 4]
+print(lista) # TODO:  la lsita si permite datows repetidos
+print(conjunto) # TODO: el conjunto no permite datos repetidos
+
+
+print(type(conjunto))
+
+
