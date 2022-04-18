ac815861dbf1012a8ec728189dad27ccaa694cfb
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-04-17 21:17:02 -0500

    ejercicio 2

---
+++ 10.Tuplas/Ejercicio2.py
@@ -0,0 +1,10 @@
+'''
+Escribir una tupla que tenga las letras del alfabeto. Luego, debes pedir al usuario un número, el que haya ingresado, es la letra que debe imprimir el programa
+
+'''
+tupla= ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V','W','X','Y','Z')
+
+numero = int(input('ingrese un numero para obener el Letra: '))
+
+
+print('la letra es: ', tupla[numero-1])
