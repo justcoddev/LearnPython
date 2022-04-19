040b6221a75c7ee4fb210a0e513c56926ad1dc3c
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-04-18 23:50:27 -0500

    Bucle WHILE

---
+++ 11.Bucles/While.py
@@ -0,0 +1,7 @@
+i = 0
+# TODO: mientras
+while i < 10:
+    i += 1
+    print(f'Estoy iterando, voy por el salto: {i}')
+
+# TODO: NOTA: Recordar siempre colocar iterador, de lo contrario se genera un bucle infinito.
