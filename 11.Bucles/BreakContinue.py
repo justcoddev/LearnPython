264ce80c3290e9db816c70c7d709d331dd8074b7
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-04-20 07:37:41 -0500

    ciclo for y range y break continue

---
+++ 11.Bucles/BreakContinue.py
@@ -0,0 +1,11 @@
+for i in range(1, 11):
+    print(i)
+    if i == 5:  # TODO: si cada que el iterador salga comparará si es igua la 5 y dependiendo si es true o false se cumple o n ola sentencia break
+        break
+
+
+# TODO:  continue
+for i in range(1, 11):
+    if i == 6: # TODO: si  el iterador es igual a 6 y por ense continue omite mostrar el valor y continua
+        continue
+    print(i)
