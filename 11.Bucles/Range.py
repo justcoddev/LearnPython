266ab4476c5997467a8315a54af0bac1f355e7ae
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-04-20 07:37:07 -0500

    ciclo for y range

---
+++ 11.Bucles/Range.py
@@ -0,0 +1,11 @@
+
+for i in range(1,10): # TODO: punto de inicio y punto final
+    print(i)
+
+    # TODO:  ahora mostrando cada 2 numeros
+for i in range(1,11, 3): # TODO: punto de inicio y punto final, separacion
+    print(i)
+    
+
+for i in range(-10, -1): # TODO: punto de inicio y punto final, separacion
+    print(i)
