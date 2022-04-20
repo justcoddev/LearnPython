979a74dc2d5e0ebdf0855618e70b20a6e70ff85f
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-04-20 15:20:58 -0500

    funciones creadas

---
+++ 12.Funciones/FuncionesCreadas.py
@@ -0,0 +1,15 @@
+'''
+def <nombre de la funcion>():
+    <sentencia>
+'''
+#--------------------------------------
+def saludo():
+    print('Hola Teddy')
+    print('Hola x2')
+    
+saludo()
+#--------------------------------------
+def tabla7():
+    for i in range(10):
+        print(f'7 x {i} = {i*7}')
+tabla7()
