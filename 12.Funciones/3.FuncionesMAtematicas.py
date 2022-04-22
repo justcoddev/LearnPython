c64bb3c0d8c589affda34624ff9a04345ca87323
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-04-21 23:27:18 -0500

    Funciones matematicas

---
+++ 12.Funciones/3.FuncionesMAtematicas.py
@@ -0,0 +1,15 @@
+import math 
+import random
+
+print(math.pow(2,5)) # TODO: eleva un numero a un exponente POW en decimal
+print(math.sqrt(25)) # TODO:  saca la raiz cuadrada SQRT en decimal
+print(math.isqrt(25)) # TODO:  saca la raiz cuadrada ISQRT en entero
+print(math.sin(90)) # TODO:  funciones trigonometricas 
+print(math.cos(90)) # TODO:  funciones trigonometricas 
+print(math.tan(90)) # TODO:  funciones trigonometricas 
+print(math.factorial(5)) # TODO:  saca el factorial
+
+#-------------------------------------------------
+
+print(random.randint(1,100)) # TODO: toam un numero al azar de 1 a 100
+
